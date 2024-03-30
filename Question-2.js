05b4811fe743e33af31a2a4987ddc8bf72c60d38
jasonokingo <jason.okingo@student.moringaschool.com>
2024-03-30 14:05:46 +0300

    Feat:Updated-Question-2

--- Question-2.js
+++ Question-2.js
@@ -1,15 +1,17 @@
+//Create a function that inputs the two numbers 
 function numbers(num1,num2) {
-    let arrayOfnumbers = [];
-        if(num1 <= num2){
-        for(i = num1;i <= num2; i++)
-        arrayOfnumbers.push(i);
+    let arrayOfnumbers = []; //Create an empty array which will hold the numbers
+        if(num1 <= num2){ 
+        for(i = num1;i <= num2; i++)// Create a loop that shows If num 1 is less than or equal to num2, it starts counting all the numbers from num 1 to num2 and increases by 1
+        arrayOfnumbers.push(i);//Then we push the changes to the empty array
         } else {
             for(i = num1;i >= num2; i--)
-        arrayOfnumbers.push(i);
+            // Create a loop that shows If num1 is greater than or equal to num2, it starts counting from it starts counting all the numbers from num 1 to num2 and decreases by 1
+        arrayOfnumbers.push(i);//Then we push the changes to the empty array
         }
-    return arrayOfnumbers;
+    return arrayOfnumbers;//Return the array with function we created
 }
 
-
-console.log(numbers(7, 4)); 
+//Then log in the numbers
+console.log(numbers(4, 7)); 
 console.log(numbers(-4, 7));
